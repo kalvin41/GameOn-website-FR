397e4a2625f04dee68e4efe782e4bce63e1a7994
Kalvin <kalvinmachful@hotmail.com>
2024-07-07 10:46:30 +0200

    Implémentation entrée du form + ajout message d'erreur

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,6 +12,7 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeModal = document.querySelector(".close");
+const form = document.getElementById("reserve"); //select form
 
 
 // launch modal event
@@ -23,8 +24,78 @@ function launchModal() {
 }
 
 
-// Événement pour fermer le modal et rediriger vers la page d'accueil
+// Close modal
 closeModal.addEventListener("click", function() {
-  modalbg.style.display = "none"; // Masquer le modal
-  window.location.href = 'index.html'; // Rediriger vers la page d'accueil
+  modalbg.style.display = "none"; // hide modal
+  window.location.href = 'index.html'; // Redirect home page
 });
+
+// validate form
+form.addEventListener('submit', function(event) {
+  event.preventDefault()
+  if (validateForm()) {
+      modalbg.style.display = "none";
+      window.location.href = 'index.html';
+  }
+});
+
+function validateForm() {
+    let valid = true;
+
+    const first = document.getElementById("first");
+    const last = document.getElementById("last");
+    const email = document.getElementById("email");
+    const birthdate = document.getElementById("birthdate");
+    const quantity = document.getElementById("quantity");
+    const location = document.querySelector('input[name="location"]:checked');
+    const checkbox1 = document.getElementById("checkbox1");
+
+    clearErrors(); // if condition is true than clear the error message otherwise the error keep remaining
+
+    if (first.value.trim().length < 2 || first.value.trim() === "") {
+        valid = false;
+        showError("first-error", "Prénom doit contenir au moins 2 caractères.");
+    }
+    if (last.value.trim() === "" || last.value.trim().length < 2) {
+        valid = false;
+        showError("last-error", "Nom  doit contenir au moins 2 caractères.");
+    }
+    if (!email.value.includes("@") || !email.value.includes(".")) {
+        valid = false;
+        showError("email-error", "E-mail est invalide.");
+    }
+    if (!isValidDate(birthdate.value)) {
+      valid = false;
+      showError("birth-error", "Date de naissance est invalide.");
+  }
+    if (quantity.value === "" || isNaN(quantity.value)) {
+        valid = false;
+        showError("quantity-error", "Veuillez entrer une quantité numérique valide.");
+    }
+    if (!location) {
+        valid = false;
+        showError("location-error", "Veuillez choisir une location.");
+    }
+    if (!checkbox1.checked) {
+        valid = false;
+        showError("checkbox1-error", "Vous devez accepter les conditions d'utilisation.");
+    }
+
+    return valid;
+}
+
+function showError(elementId, message) { // Display an error message
+    const errorElement = document.getElementById(elementId);
+    errorElement.textContent = message;
+}
+
+function clearErrors() { // Remove error message if the condition true
+    const errorMessages = document.querySelectorAll(".error-message");
+    errorMessages.forEach(function(error) {
+        error.textContent = "";
+    });
+}
+function isValidDate(dateString) { // Check if is a  valide date
+  const date = new Date(dateString);
+  return !isNaN(date.getTime());
+}
